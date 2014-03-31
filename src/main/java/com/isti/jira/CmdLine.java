1f5ca6b61271998d4ea816a1e2aa707ada8eb6a3
andrew cooke <a.cooke@isti.com>
2014-03-31 15:44:23 -0300

    add command line options; fix pom conflicts

--- src/main/java/com/isti/jira/CmdLine.java
+++ src/main/java/com/isti/jira/CmdLine.java
@@ -2,15 +2,40 @@ package com.isti.jira;
 
 import com.atlassian.jira.rest.client.JiraRestClient;
 import com.atlassian.jira.rest.client.internal.jersey.JerseyJiraRestClientFactory;
+import io.airlift.command.Cli;
+import io.airlift.command.Command;
+import io.airlift.command.Help;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 
 public class CmdLine {
 
-    public static void main(String[] args) throws Exception {
-        JerseyJiraRestClientFactory factory = new JerseyJiraRestClientFactory();
-        URI jiraServerUri = new URI("http://localhost:8081/jira");
-        JiraRestClient restClient = factory.create(jiraServerUri, null);
+    public static void main(String[] args) {
+        Cli.CliBuilder<Runnable> builder = Cli.<Runnable>builder("CmdLine")
+                .withDescription("Command line tool for Jira")
+                .withDefaultCommand(Help.class)
+                .withCommands(Help.class, CreateUser.class);
+        Cli<Runnable> parser = builder.build();
+        parser.parse(args).run();
+    }
+
+    private static class Connection {
+
+        public JiraRestClient getClient() throws URISyntaxException {
+            JerseyJiraRestClientFactory factory = new JerseyJiraRestClientFactory();
+            URI jiraServerUri = new URI("http://localhost:8081/jira");
+            return factory.create(jiraServerUri, null);
+        }
+
+    }
+
+    @Command(name="add-user", description="Add a Jenkins user to Jira")
+    public static class CreateUser extends Connection implements Runnable {
+
+        public void run() {
+            System.out.println("create user");
+        }
     }
 
 }
